d5046789998aab10c1e31bb5312dd0b6dcb6d0b2
graydon18 <graydol18@gmail.com>
2025-10-20 10:16:39 -0400

    Style and organize home page buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,31 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
-      <h1 className="text-9xl font-jersey animate-swell text-denim text-shadow-lg">Strike Journey</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <Link to="/leagues">
-          <button className="bg-blue-500 text-white px-6 py-4 rounded-lg text-xl">
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 space-y-8">
+      <h1 className="text-7xl sm:text-8xl md:text-9xl text-center mb-12 font-jersey animate-swell text-denim text-shadow-lg">Strike Journey</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-2xl">
+        <Link to="/leagues" className="w-full">
+          <button className="w-full aspect-[3/1] bg-pig hover:bg-pig-shadow text-white rounded-2xl text-2xl font-semibold 
+          shadow-md transition-colors duration-300 ease-in-out">
             Leagues
           </button>
         </Link>
-        <Link to="/tournaments">
-          <button className="bg-green-500 text-white px-6 py-4 rounded-lg text-xl">
+        <Link to="/tournaments" className="w-full">
+          <button className="w-full aspect-[3/1] bg-salmon hover:bg-salmon-shadow text-white rounded-2xl text-2xl font-semibold 
+          shadow-md transition-colors duration-300 ease-in-out">
             Tournaments
           </button>
         </Link>
-        <Link to="/open-bowling">
-          <button className="bg-yellow-500 text-white px-6 py-4 rounded-lg text-xl">
+        <Link to="/open-bowling" className="w-full">
+          <button className="w-full aspect-[3/1] bg-aqua hover:bg-aqua-shadow text-white rounded-2xl text-2xl font-semibold 
+          shadow-md transition-colors duration-300 ease-in-out">
             Open Bowling
-          </button>
+          </button> 
         </Link>
-        <Link to="/statistics">
-          <button className="bg-purple-500 text-white px-6 py-4 rounded-lg text-xl">
+        <Link to="/statistics" className="w-full">
+          <button className="w-full aspect-[3/1] bg-peacock hover:bg-peacock-shadow text-white rounded-2xl text-2xl font-semibold 
+          shadow-md transition-colors duration-300 ease-in-out">
             Statistics
           </button>
         </Link>
